108968db45be58f04e20d2319a6da93c7b770fca
Alexander Akimov <ext-alexander.akimov@nokia.com>
2010-07-15 11:36:27 +0300

    Printing of time could be dangerous

--- src/signond/signontrace.h
+++ src/signond/signontrace.h
@@ -88,8 +88,7 @@ namespace SignOn {
             if (!m_pInstance->m_outputFile.isOpen())
                 m_pInstance->m_outputFile.open(QIODevice::Append);
 
-            m_pInstance->m_writeStream << QString(QLatin1String("%1 %2: %3\n"))
-                . arg(QDateTime::currentDateTime().toString())
+            m_pInstance->m_writeStream << QString(QLatin1String("%1: %2\n"))
                 . arg(QLatin1String(msgType))
                 . arg(QLatin1String(msg));
             m_pInstance->m_outputFile.close();
