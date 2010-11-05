12c634a6dfbba6c8d3c633853fff57be43a8ca7b
Alberto Mardegan <alberto.mardegan@nokia.com>
2010-11-05 11:26:26 +0200

    Introduce the SignOnExtension library
    Allow signond to load plugins. This commit adds the AbstractKeyManager object,
    which can be implemented to provide signond with keys to access the secure
    storage.

---
+++ lib/signond/SignOn/SignOn.pro
@@ -0,0 +1,37 @@
+include( ../../../common-project-config.pri )
+include( $${TOP_SRC_DIR}/common-vars.pri )
+
+TEMPLATE = lib
+TARGET = signon-extension
+
+HEADERS = \
+    abstract-key-manager.h \
+    extension-interface.h
+
+INCLUDEPATH += \
+    ..
+
+SOURCES += \
+    abstract-key-manager.cpp
+
+QT += core
+QT -= gui
+
+QMAKE_CXXFLAGS += \
+    -fno-exceptions \
+    -fno-rtti
+
+DEFINES += QT_NO_CAST_TO_ASCII QT_NO_CAST_FROM_ASCII
+include( $${TOP_SRC_DIR}/common-installs-config.pri )
+
+headers.files = \
+    AbstractKeyManager \
+    abstract-key-manager.h \
+    ExtensionInterface \
+    extension-interface.h
+headers.path = $${INSTALL_PREFIX}/include/$${TARGET}/SignOn
+INSTALLS += headers
+
+pkgconfig.files = SignOnExtension.pc
+pkgconfig.path = $${INSTALL_PREFIX}/lib/pkgconfig
+INSTALLS += pkgconfig
---
+++ lib/signond/SignOn/abstract-key-manager.h
@@ -0,0 +1,136 @@
+/*
+ * This file is part of signon
+ *
+ * Copyright (C) 2010 Nokia Corporation.
+ *
+ * Contact: Alberto Mardegan <alberto.mardegan@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+#ifndef SIGNOND_ABSTRACT_KEY_MANAGER_H
+#define SIGNOND_ABSTRACT_KEY_MANAGER_H
+
+#include <SignOn/extension-interface.h>
+
+#include <QObject>
+#include <QString>
+
+namespace SignOn {
+
+typedef QByteArray Key;
+
+/*!
+ * @class AbstractKeyManager.
+ * @brief Base class for KeyManager implementations.
+ *
+ * The AbstractKeyManager is the base class for implementing signond key
+ * managers. A key manager is an object which signond uses to get the keys
+ * needed to access the encrypted storage. It emits signals whenever new keys
+ * exist, when keys are disabled or removed. For example, a simple password
+ * based key manager would:
+ * @li query the user for the password when queryKeys() is called.
+ * @li emit keyInserted() whenever the user enters the password.
+ * @li if the password automatically expires after some time, emit keyDisabled
+ * @li if the user changes the password, emit keyInserted() with the new
+ * passord, and keyRemoved() for the old one.
+ */
+class AbstractKeyManager: public QObject
+{
+    Q_OBJECT
+
+public:
+    /*!
+     * Constructor.
+     */
+    explicit AbstractKeyManager(QObject *parent = 0);
+    virtual ~AbstractKeyManager();
+
+    /*!
+     * This method initializes the key manager. Implementations can start
+     * emitting signals during this method's execution.
+     */
+    virtual void setup() = 0;
+
+    /*!
+     * signond calls this method if a key (typically a new key emitted with
+     * the keyInserted() signal) needs to be authorized. Authorizing a key
+     * means allowing the key to be used to mount the secure storage. The base
+     * implementation of this method just emits the keyAuthorized() signal
+     * denying the authorization, but specific implementations could delegate
+     * the authorization to the user.
+     * @param key The key that needs authorization.
+     * @param message An optional message that might be shown to the user.
+     */
+    virtual void authorizeKey(const Key &key,
+                              const QString &message = QString::null);
+
+    /*!
+     * signond calls this method when there are no active keys. This might
+     * happen when signond is just starting, or when all existing keys have
+     * been disabled (which would cause the secure storage to be unmounted).
+     * If the key manager <em>must</em> emit keyInserted() in response to this
+     * call, either with a valid key or with an empty one.
+     */
+    virtual void queryKeys();
+
+    /*!
+     * Get a description for the key.
+     * @param key The key whose name is to be returned.
+     * @returns A description or name that could be shown to the user to
+     * identify the key.
+     */
+    virtual QString describeKey(const Key &key);
+
+Q_SIGNALS:
+    /*!
+     * Emitted when a new key is available. If the key is not yet known to
+     * signond, signond will call authorizeKey on the key managers before
+     * accepting it.
+     * @param key The new key.
+     */
+    void keyInserted(const Key key);
+
+    /*!
+     * Emitted when a key is disabled. For instance, this signal can be
+     * emitted when a password expires or when a hardware key is removed.
+     * @param key The key which has been disabled.
+     */
+    void keyDisabled(const Key key);
+
+    /*!
+     * Emitted when a key is to be removed. This means that the key will never
+     * be used again to access the secure storage; if it will be inserted
+     * again, it will have to be re-authenticated.
+     * It is not necessary to emit keyDisabled() before keyRemoved(): the key
+     * will be disabled anyway when keyRemoved is emitted.
+     * @param key The key which has to be removed.
+     */
+    void keyRemoved(const Key key);
+
+    /*!
+     * The key manager emits this when it has decided whether to authorized
+     * the key. If more than one key manager is installed, signond will
+     * authorized the key as soon as the first key manager emits this signal
+     * with authorized set to true.
+     * @param key The key which underwent the authorization step.
+     * @param authorized The result of the authorization.
+     */
+    void keyAuthorized(const Key key, bool authorized);
+};
+
+} // namespace
+
+#endif // SIGNOND_ABSTRACT_KEY_MANAGER_H
+
